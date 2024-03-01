63082a2a9afac6fd01058ff2377e4510c11d0f90
Nina Weber <weber_nina@web.de>
2024-03-01 12:48:34 +0100

    add Form component

---
+++ src/components/Form.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function Form({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // const [goodWeather, setGoodWeather] = useState(false);
+
+    const formElements = event.target.elements;
+    const name = formElements.name.value;
+    const isForGoodWeather = formElements.checkbox.checked;
+
+    // const newActivity = [{}]
+    onAddActivity(newActivity);
+
+    // setActivity(formElements.name.value);
+    // setGoodWeather(formElements.checkbox.checked);
+
+    event.target.reset();
+    formElements.name.focus();
+  }
+
+  return (
+    <>
+      <h1>Add new Activity:</h1>
+      <form>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" />
+        <label htmlFor="checkbox">Good-weather activity:</label>
+        <input type="checkbox" id="checkbox" name="checkbox" />
+        <button type="submit" onSubmit={handleSubmit}>
+          Submit
+        </button>
+      </form>
+    </>
+  );
+}
